31989d4bf05c66ed707bb069c6fca40cd3017a8f
sonneanjali2001-dev <anjalisonni65@gmail.com>
2025-11-08 18:02:47 +0530

    new

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+import pytest 
+from calculator import add, sub,mul,div
+def test_add():
+    assert add(2,3) == 5
+def test_sub():
+    assert sub(4,2) == 2
+def test_mul():
+    assert mul(2,3) == 6
+def test_div():
+    assert div(10,5) == 2
+
